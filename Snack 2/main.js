a650c8ab1918a948c416f0010d4fa7299f921cee
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-08-29 15:30:16 +0200

    snack 2 complete

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -5,3 +5,39 @@
 L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
 */
+
+/* Strumenti:
+    - prompt x2
+    - due collegamenti al DOC
+    - if/else per vedere la parola più lunga
+    - stampare in pagina le due
+ */
+
+// prompt 1
+const primaParola = prompt('inserisci la prima parola');
+
+// prompt 2
+const secondaParola = prompt('inserisci la seconda parola');
+
+// collego al DOC
+const parolaCortaDOC = document.getElementById('parolaCorta');
+const parolaLungaDOC = document.getElementById('parolaLunga');
+
+
+if (primaParola.length > secondaParola.length) {
+
+    parolaCortaDOC.append(secondaParola);
+    parolaLungaDOC.append(primaParola);
+    
+} else if (primaParola.length < secondaParola.length) {
+
+    parolaLungaDOC.append(secondaParola);
+    parolaCortaDOC.append(primaParola);
+    
+} else{
+
+    parolaCortaDOC.append(secondaParola);
+    parolaLungaDOC.append(primaParola)
+    parolaLungaDOC.insertAdjacentHTML('afterend','sono della stessa lunghezza')
+    
+}
